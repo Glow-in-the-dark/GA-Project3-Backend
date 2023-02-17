8f57e644bd933892eb7d95a99fddda4ab9c1e2d0
Gavin <Gavinlowa111989@gmail.com>
2023-02-17 11:55:08 +0800

    upload images & README

--- server.js
+++ server.js
@@ -19,29 +19,29 @@ app.use("/volunteer-slots", volunteerSlots);
 // Route to all user endpoints
 app.use("/users", users);
 
-// // Seed data:
-// const seed = require("./models/SeedData");
-// const VolunteerSlots = require("./models/VolunteerSlots");
+// Seed data:
+const seed = require("./models/SeedData");
+const VolunteerSlots = require("./models/VolunteerSlots");
 
-// app.get("/seed", async (req, res) => {
-//   // seed.forEach((item) => {
-//   //   const newItem = {
-//   //     date: item.date,
-//   //     orig_slots_available: item.orig_slots_available,
-//   //     sign_ups: item.sign_ups,
-//   //   };
-//   // });
-//   console.log(seed);
-//   await VolunteerSlots.create(seed, (err, createdUsers) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(400).json({ status: "error", message: "seeding error" });
-//     } else {
-//       // logs created users
-//       console.log(createdUsers);
-//     }
-//   });
-// });
+app.get("/seed", async (req, res) => {
+  seed.forEach((item) => {
+    const newItem = {
+      date: item.date,
+      orig_slots_available: item.orig_slots_available,
+      sign_ups: item.sign_ups,
+    };
+  });
+  console.log(seed);
+  await VolunteerSlots.create(seed, (err, createdUsers) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ status: "error", message: "seeding error" });
+    } else {
+      // logs created users
+      console.log(createdUsers);
+    }
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server started on Port ${process.env.PORT}`);
